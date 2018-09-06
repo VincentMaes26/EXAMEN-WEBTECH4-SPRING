061aa9e9f6064a734a77f7a9f6b5f335d88f2cf9
Vincent Maes <32563747+VincentMaes26@users.noreply.github.com>
2018-09-06 15:40:52 +0200

    API call works

--- src/main/java/edu/ap/spring/services/JokeService.java
+++ src/main/java/edu/ap/spring/services/JokeService.java
@@ -1,7 +1,7 @@
 package edu.ap.spring.services;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
+import javax.json.JsonObject;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,16 +11,17 @@ import edu.ap.spring.jpa.Joke;
 public class JokeService {
 	private String baseURL = "http://api.icndb.com/jokes/random";
 	
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 	
 	public Joke getJoke(String firstName, String lastName) {
 		RestTemplate restTemplate = new RestTemplate();
-		Joke newJoke = new Joke();
-		String url = baseURL + "?firstName="+firstName+"&lastName="+lastName;
-		newJoke = restTemplate.getForObject(url, Joke.class);
+		Joke newJoke;
+		String urlString = baseURL + "?firstName="+firstName+"&lastName="+lastName;
+		
+		String jokeObjectString = restTemplate.getForObject(urlString, String.class);
+		String[] stringArray = jokeObjectString.split("\"");
+		String jokeString = stringArray[11];
+		System.out.println(jokeString);
+		newJoke = new Joke(jokeString);
 		return newJoke;
 	}
 }
